a0fede884cd24a46f983361e994f93d76a54ad37
Lucas Chaves <60762919+lucas-chave@users.noreply.github.com>
2025-07-29 21:32:59 +0000

    adicionando exercicios da primeira parte do capitulo 3

--- Chapter3/chap3_1.js
+++ Chapter3/chap3_1.js
@@ -8,15 +8,24 @@ function List() {
    this.insert = insert;
    this.append = append;
    this.remove = remove;
+   this.contains = contains;
    this.front = front;
    this.end = end;
    this.prev = prev;
    this.next = next;
-   this.length = length;
    this.currPos = currPos;
    this.moveTo = moveTo;
    this.getElement = getElement;
    this.length = length;
+}
+
+function contains(element) {
+   for (let i = 0; i < this.dataStore.length; ++i) {
+      if (this.dataStore[i] == element) {
+         return true;
+      }
+   }
+   return false;
 }
 
 function append(element) {
@@ -24,7 +33,7 @@ function append(element) {
 }
 
 function find(element) {
-   for (var i = 0; i < this.dataStore.length; ++i) {
+   for (let i = 0; i < this.dataStore.length; ++i) {
       if (this.dataStore[i] == element) {
          return i;
       }
@@ -33,9 +42,9 @@ function find(element) {
 }
 
 function remove(element) {
-   var foundAt = this.find(element);
+   let foundAt = this.find(element);
    if (foundAt > -1) {
-      this.dataStore.splice(foundAt,1);
+      this.dataStore.splice(foundAt, 1);
       --this.listSize;
       return true;
    }
@@ -43,13 +52,113 @@ function remove(element) {
 }
 
 function toString() {
-    return this.dataStore;
+   return this.dataStore;
 }
 
-var names = new List();
-names.append("Cynthia");
+function length() {
+   return this.listSize;
+}
+
+function clear() {
+   delete this.dataStore;
+   this.dataStore = [];
+   this.listSize = this.pos = 0;
+}
+
+
+function insert(element, after) {
+   let insertPos = this.find(after);
+   if (insertPos > -1) {
+      this.dataStore.splice(insertPos + 1, 0, element);
+      ++this.listSize;
+      return true;
+   }
+   return false;
+}
+
+function contains(element) {
+   for (let i = 0; i < this.dataStore.length; ++i) {
+      if (this.dataStore[i] == element) {
+         return true;
+      }
+   }
+   return false;
+}
+
+function front() {
+   this.pos = 0;
+}
+
+function end() {
+   this.pos = this.listSize - 1;
+}
+
+function prev() {
+   if (this.pos > 0) {
+      --this.pos;
+   }
+}
+
+function next() {
+   if (this.pos < this.listSize - 1) {
+      ++this.pos;
+   }
+}
+
+function currPos() {
+   return this.pos;
+}
+
+function moveTo(position) {
+   this.pos = position;
+}
+
+function getElement() {
+   return this.dataStore[this.pos];
+}
+
+let names = new List();
+// names.append("Cynthia");
+// names.append("Raymond");
+// names.append("Barbara");
+// console.log(names.length());
+// names.remove("Raymond");
+// console.log(names.length());
+// console.log(names.toString());
+// names.insert('Fulano', 'Cynthia');
+// console.log(names.toString());
+// console.log(names.contains('Example'));
+// console.log(names.contains('Cynthia'));
+// names.clear();
+// console.log(names.toString(), 'clear');
+
+names.append("Clayton");
 names.append("Raymond");
-names.append("Barbara");
-print(names.toString());
-names.remove("Raymond");
-print(names.toString());
+names.append("Cynthia");
+names.append("Jennifer");
+names.append("Bryan");
+names.append("Danny");
+// console.log(names.toString());
+// names.front();
+// console.log(names.getElement());
+// console.log(names.toString());
+// names.next();
+// console.log(names.getElement());
+// console.log(names.toString());
+// names.next();
+// names.next();
+// names.prev();
+// console.log(names.getElement());
+// console.log(names.toString());
+// names.end()
+// console.log(names.getElement());
+// console.log(names.currPos());
+// names.moveTo(2)
+// console.log(names.getElement());
+// console.log(names.currPos());
+
+// console.log(names.toString(), names.length(), names.currPos());
+
+for (names.front(); names.currPos() < names.length(); names.next()) {
+   console.log(names.getElement());
+}
